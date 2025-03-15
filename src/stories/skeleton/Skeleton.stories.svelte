0ffd189e24a4b0fe2cdd9f15bdf8f50563bf73f0
whynot830 <aminnurulla752@gmail.com>
2025-03-16 02:53:05 +0300

    feat(ui-1): Skeleton story

---
+++ src/stories/skeleton/Skeleton.stories.svelte
@@ -0,0 +1,37 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+
+  import { Skeleton } from '$lib/components/ui'
+
+  const { Story } = defineMeta({
+    title: 'Components/Skeleton',
+    component: Skeleton,
+    parameters: {
+      docs: {
+        description: {
+          component: 'Use to show a placeholder while content is loading'
+        }
+      }
+    }
+  })
+</script>
+
+<Story name="Default">
+  <div class="space-y-4">
+    <div class="flex items-center space-x-4">
+      <Skeleton class="size-12 rounded-full" />
+      <div class="space-y-2">
+        <Skeleton class="h-4 w-[250px]" />
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div class="space-y-2">
+      <Skeleton class="h-24 w-full" />
+      <div class="grid grid-cols-3 gap-2">
+        <Skeleton class="h-4" />
+        <Skeleton class="h-4" />
+        <Skeleton class="h-4" />
+      </div>
+    </div>
+  </div>
+</Story>
