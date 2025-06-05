03b607aef58c63200dbd3c96fe1c410bffbc0743
whynot <aminfury@mail.ru>
2025-06-05 20:56:04 +0300

    feat(ui-2): Sheet with parking info component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { Loader2Icon } from '@lucide/svelte'
-  import { latLng, type HeatMapOptions } from 'leaflet'
+  import { latLng } from 'leaflet'
   import { Circle, Map, Popup, TileLayer } from 'sveaflet'
   import { Heat } from 'sveaflet-heat'
 
@@ -8,8 +8,14 @@
   import { LEAFLET_HEATMAP_OPTIONS } from '@/shared/const'
   import { valueToColor } from '@/shared/lib'
   import { theme } from '@/shared/stores'
-
-  let { data } = $props()
+  import {
+    Button,
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle
+  } from '@/shared/ui'
 
   const getOccupancyQuery = parkingModel.useGetCurrentOccupancy()
   const getParkingsMetaQuery = parkingModel.useGetParkingsMeta()
@@ -21,38 +27,48 @@
     error: occupancyDataError
   } = $derived($getOccupancyQuery)
 
-  // const {
-  //   data: parkingsMetaData,
-  //   isLoading: isParkingsMetaLoading,
-  //   isError: isParkingsMetaError,
-  //   error: parkingsMetaError
-  // } = $derived($getParkingsMetaQuery)
+  const {
+    data: parkingsMetaData,
+    isLoading: isParkingsMetaLoading,
+    isError: isParkingsMetaError,
+    error: parkingsMetaError
+  } = $derived($getParkingsMetaQuery)
 
-  let latLngs = $derived(
-    occupancyData?.occupancy.map((parking) =>
-      latLng({
+  let open = $state<boolean>(false)
+  let selectedParking = $state<parkingModel.ParkingMeta>()
+
+  const selectParking = (id: number) => {
+    selectedParking = parkingsMetaData![id]
+    open = true
+  }
+
+  let parkingsOccupancy = $derived(
+    occupancyData?.occupancy.map((parking) => ({
+      parkingId: parking.parkingId,
+      latLng: latLng({
         lat: parking.coordinates.latitude,
         lng: parking.coordinates.longitude,
         alt: parking.occupancy
       })
-    ) || []
+    })) || []
   )
 </script>
 
-{#if isOccupancyDataLoading}
+{#if isOccupancyDataLoading || isParkingsMetaLoading}
   <div class="h-full w-full">
     <div class="flex h-7/8 w-full items-center justify-center">
       <Loader2Icon class="size-[20%] max-w-40 animate-spin" />
     </div>
   </div>
-{:else if isOccupancyDataError}
+{:else if isOccupancyDataError || isParkingsMetaError}
   <div class="h-full w-full">
     <div class="flex h-7/8 w-full items-center justify-center">
       <p class="text-destructive text-center text-xl font-semibold">
         <span>Something went wrong...</span>
         <br />
         <span
-          >Message: {occupancyDataError.message ?? 'Unknown error'} ({occupancyDataError.status})</span
+          >Message: {occupancyDataError?.message ?? parkingsMetaError?.message ?? 'Unknown error'} ({occupancyDataError?.status ??
+            parkingsMetaError?.status})</span
         >
       </p>
     </div>
@@ -70,24 +86,99 @@
             '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a>'
         }}
       />
-      {#each latLngs as ap}
+      {#each parkingsOccupancy as { parkingId, latLng }}
         <Circle
-          options={{ radius: 50, color: valueToColor(ap.alt || 0), opacity: 0.5 }}
-          latLng={ap}
+          options={{ radius: 50, color: valueToColor(latLng.alt || 0), opacity: 0.5 }}
+          {latLng}
         >
           <Popup>
-            <div>
-              <span class="">
-                {ap.alt?.toFixed(2)}
+            <div class="flex flex-col gap-2">
+              <span class="font-medium">
+                Occupancy: {((latLng.alt || 0) * 100).toFixed(2)}%
               </span>
+              <Button variant="secondary" onclick={() => selectParking(parkingId)}>Show info</Button
+              >
             </div>
           </Popup>
         </Circle>
       {/each}
 
-      {#if latLngs.length > 0}
-        <Heat {latLngs} options={LEAFLET_HEATMAP_OPTIONS} />
+      {#if parkingsOccupancy.length > 0}
+        <Heat
+          latLngs={parkingsOccupancy.map((parking) => parking.latLng)}
+          options={LEAFLET_HEATMAP_OPTIONS}
+        />
       {/if}
     </Map>
+
+    <Sheet {open} onOpenChange={(value) => (open = value)}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>General info</SheetTitle>
+          <SheetDescription>Parking metadata</SheetDescription>
+        </SheetHeader>
+
+        <div class="flex flex-col px-4">
+          <table
+            class="[&_td:nth-child(odd)]:text-muted-foreground w-full border-collapse text-sm
+            [&_td]:py-2 [&_td:nth-child(even)]:ps-2 [&_td:nth-child(even)]:font-medium [&_td:nth-child(odd)]:pe-2
+            [&_tr]:border-b"
+          >
+            <tbody>
+              <tr class="border-t">
+                <td>Name</td>
+                <td>
+                  <span>
+                    {selectedParking?.nameEn}
+                    <br />
+                    ({selectedParking?.nameRu})
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td>Address</td>
+                <td>
+                  {selectedParking?.addressStreetEn}
+                  <br />
+                  ({selectedParking?.addressStreetRu})
+                </td>
+              </tr>
+              <tr>
+                <td>Subway</td>
+                <td>
+                  {selectedParking?.subwayEn} ({selectedParking?.subwayRu})
+                </td>
+              </tr>
+              <tr>
+                <td>Coordinates</td>
+                <td>
+                  {selectedParking?.latitude}&deg; N,
+                  {selectedParking?.longitude}&deg; E
+                </td>
+              </tr>
+
+              <tr>
+                <td>Total spaces</td>
+                <td>
+                  {selectedParking?.totalSpaces}
+                </td>
+              </tr>
+              <tr>
+                <td>Common spaces</td>
+                <td>
+                  {selectedParking?.commonSpaces}
+                </td>
+              </tr>
+              <tr>
+                <td>Handicapped spaces</td>
+                <td>
+                  {selectedParking?.handicappedSpaces}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </SheetContent>
+    </Sheet>
   </div>
 {/if}
